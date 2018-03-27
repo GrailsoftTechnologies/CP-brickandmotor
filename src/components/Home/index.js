f881f93976cfeb357108ddc5c31dc11bf9725a8c
David Cuthell <dcc1447@gmail.com>
2018-03-26 21:37:27 -0700

    changed some styling stuff to fit screen

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,15 +7,19 @@ class Home extends Component {
     return (
       <main className='wrapper'>
         <section className='section parallax bg1'>
-          <h1>BRICK</h1>
-          <h2>AND</h2>
-          <h1>MOTOR</h1>
-          <h2>WOOD FIRED PIZZA</h2>
+          <div className="lineStyle1">BRICK</div>
+          <div className="lineStyle2">AND</div>
+          <div className="lineStyle1">MOTOR</div>
+          <div className="lineStyle2">WOOD FIRED PIZZA</div>
         </section>
         <section className='section static'>
-          <h1>PIZZA?</h1>
+          <div className="lineStyle3">CALL</div>
+          <div className="lineStyle3">FIND</div>
+          <div className="lineStyle3">ORDER</div>
         </section>
         <section className='section parallax bg2'>
+          <div className="lineStyle2">PIZZA?</div>
+          <div className="lineStyle2">PIZZA?</div>
         </section>
       </main>
     );
