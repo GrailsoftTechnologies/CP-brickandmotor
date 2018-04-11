c0e313a2a3db9f903c4490732229b108c86ed00c
nr <nicholas.raethke@gmail.com>
2018-04-11 14:23:36 -0700

    create some initial contact content

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,10 +6,13 @@ render() {
   return(
     <main className='wrapper'>
       <section>
-        <h1>A</h1>
-        <h2>BUNCH</h2>
-        <h1>OF</h1>
-        <h2>CONTACT</h2>
+        <h2>Call- 971.988.6575 </h2>
+        <hr />
+        <h2><a href="https://www.amazon.com/restaurants/brick-and-motor-portland/d/B079J6MDKB?ref_=amzrst_b_B079J6MDKB_23" alt="link to amazon pizza delivery">Amazon Delivery</a></h2>
+        <hr />
+        <h2>Map will Go here</h2>
+        <hr />
+        <h2>Follow</h2>
         <h2>STUFF</h2>
         <h2>HERE</h2>
       </section>
