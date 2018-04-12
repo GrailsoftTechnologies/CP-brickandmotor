48c22178331d4fb83c93b19dfdd2e78ef7cd58b5
Rich Garrick <richg341@gmail.com>
2018-04-12 13:29:40 -0700

    update css for primeLogo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -14,11 +14,11 @@ class Home extends Component {
           <div className="lineStyle2">WOOD FIRED PIZZA</div>
         </section>
         <section className='section static'>
-          <p id="test">Deliver unto us the Pizza</p>
-          <a href="https://www.amazon.com/restaurants/brick-and-motor-portland/d/B079J6MDKB?ref_=amzrst_b_B079J6MDKB_20" target="_blank" ><img src={PrimeNow} alt='amazon restaurant delivery' id="primeLogo"></img></a>
         </section>
         <section className='section parallax bg2'>
           <div className="lineStyle2">PIZZA?</div>
+          <p id="test">Deliver unto us the Pizza</p>
+          <a href="https://www.amazon.com/restaurants/brick-and-motor-portland/d/B079J6MDKB?ref_=amzrst_b_B079J6MDKB_20" target="_blank" ><img src={PrimeNow} alt='amazon restaurant delivery' id="primeLogo"></img></a>
           <div className="lineStyle2">PIZZA?</div>
         </section>
       </main>
