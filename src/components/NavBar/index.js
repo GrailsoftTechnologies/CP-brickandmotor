da55266bffb0c74a926d8ac037e495d8e29e687c
Rich Garrick <richg341@gmail.com>
2018-04-12 14:06:57 -0700

    update media query to hide navbar icons on mobile screens

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -16,9 +16,9 @@ class NavBar extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <NavItem href="https://www.instagram.com/brickandmotorpdx/" target="_blank" rel="noopener noreferrer"><span className="footer-icon icon fa fa-instagram"></span></NavItem>
-            <NavItem href="https://www.facebook.com/brickandmotorpdx/" target="_blank" rel="noopener noreferrer"><span className="footer-icon icon fa fa-facebook-square"></span></NavItem>
-            <NavItem href="https://www.twitter.com" target="_blank" rel="noreferrer noopener"><span className="footer-icon icon fa fa-twitter-square"></span></NavItem>
+            <NavItem href="https://www.instagram.com/brickandmotorpdx/" target="_blank" rel="noopener noreferrer"><span className="footer-icon icon fa fa-instagram desktop-only"></span></NavItem>
+            <NavItem href="https://www.facebook.com/brickandmotorpdx/" target="_blank" rel="noopener noreferrer"><span className="footer-icon icon fa fa-facebook-square desktop-only"></span></NavItem>
+            <NavItem href="https://www.twitter.com" target="_blank" rel="noreferrer noopener"><span className="footer-icon icon fa fa-twitter-square desktop-only"></span></NavItem>
           </Nav>
           <Nav pullLeft>
             <NavItem href={"/Menu"} className="menuItem">Menu</NavItem>
