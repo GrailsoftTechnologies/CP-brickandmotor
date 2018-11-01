53ff38516401c4585e0f4d0c600d31669ef8d432
David Cuthell <dcc1447@gmail.com>
2018-11-01 09:28:12 -0700

    update app.js, index.js, and related css files to match new template

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import Router from './components/Router/index.js';
-import NavBar from './components/NavBar';
+import NavigationBar from './components/NavBar';
 import Footer from './components/Footer';
 import './App.css';
+import Logo from './img/brickAndMotorLogo.png';
+import Background1 from './img/image5.jpeg';
+import Background2 from './img/image4.jpeg';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar/>
-        <Router/>
-        <Footer/>
+        <NavigationBar background="black" text="white" hovercolor="grey" logo={Logo}/>
+        <Router background="black" text="white" hovercolor="grey" backgroundImage={[Background1, Background2]}/>
+        <Footer background="black" text="white" hovercolor="grey"/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
-import 'typeface-roboto';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
