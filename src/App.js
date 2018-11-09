7807c9a478e4e38231932e9b9b771c2f38371ab5
David Cuthell <dcc1447@gmail.com>
2018-11-09 18:36:37 -0500

    swap out router for ParallaxPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import Router from './components/Router/index.js';
+import ParallaxPage from './components/ParallaxPage';
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
 import './App.css';
 import Logo from './img/image2.jpeg';
-import Background1 from './img/image5.jpeg';
-import Background2 from './img/image4.jpeg';
 
 
 class App extends Component {
@@ -13,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavigationBar background="black" text="white" hovercolor="#d9d9db" logo={Logo}/>
-        <Router background="black" text="white" hovercolor="#d9d9db" backgroundImage={Background1}/>
+        <ParallaxPage background="black" text="white" hovercolor="#d9d9db"/>
         <Footer background="black" text="white" hovercolor="#d9d9db"/>
       </div>
     );
